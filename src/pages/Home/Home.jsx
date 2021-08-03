56ff351d48087f36ebc120f3224eda7c4dcb3373
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-08-03 08:36:29 +0100

    initial commits

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import './home.css'
-import Featuredupdates from '../../components/feauredupdates/Featuredupdates'
-import Chart from '../../components/Chart/Chart'
-import data from '../../Data';
-import Lgwidget from '../../components/Widget/LargeWidget/Lgwidget';
-import Smwidget from '../../components/Widget/SmallWidget/Smwidget';
-function Home() {
-
-    console.log(data.length);
-    return (
-        <div className="homeContainer">
-            <div className="homeWrapper">
-            <Featuredupdates/>
-            <Chart dataHouse = {data} dataKey = "Activer_User" title = "User Analytics"/>
-            <div className="widgetContainer">
-                <Smwidget/>
-                <Lgwidget/>
-            </div>
-            </div>
-           
-        </div>
-
-    )
-}
-
-export default Home
-=======
 import './home.css';
 
 function Home() {
@@ -38,4 +9,3 @@ function Home() {
 }
 
 export default Home
->>>>>>> admindashboard/clouddevice
