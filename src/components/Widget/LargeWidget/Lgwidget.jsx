7309d318359c98ad486c0685ce8a6c76d8701467
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-08-03 10:41:29 +0100

    adding lgwidget

--- src/components/Widget/LargeWidget/Lgwidget.jsx
+++ src/components/Widget/LargeWidget/Lgwidget.jsx
@@ -5,18 +5,74 @@ function Lgwidget() {
         <div className="lgWidgetContainer">
             <span className="title">Latest transactions</span>
             <div class="grid-container">
-                <div class="grid-item">Customer</div>
-                <div class="grid-item">Date</div>
-                <div class="grid-item">Amount</div>
-                <div class="grid-item">Status</div>
-                <div class="grid-item flex">
+                <div class="grid-item"><h4>Customer</h4></div>
+                <div class="grid-item"><h4>Date</h4></div>
+                <div class="grid-item"><h4>Amount</h4></div>
+                <div class="grid-item"><h4>Status</h4></div>
+                <div class="grid-item">
+                <div className = "flex">
                 <img src = "https://cdn.pixabay.com/photo/2021/07/14/19/18/woman-6466907__340.jpg" alt ="tell all the boys"></img>
-                <h3>Susan Carol</h3>
+                <span>Susan Carol</span>
                 </div>
-                <div class="grid-item">6</div>
-                <div class="grid-item">7</div>
-                <div class="grid-item">8</div>
-                <div class="grid-item">9</div>
+                
+                </div>
+                <div class="grid-item">
+                <div className = "flex"> <span>2 Jun 2021</span></div>
+                   
+                </div>
+                <div class="grid-item">
+                <div className = "flex"><span>$1200</span></div></div>
+                <div class="grid-item">
+                <div className = "flex"><span>Approved</span></div></div>
+                
+
+                <div class="grid-item">
+                <div className = "flex">
+                <img src = "https://cdn.pixabay.com/photo/2021/07/14/19/18/woman-6466907__340.jpg" alt ="tell all the boys"></img>
+                <span>Susan Carol</span>
+                </div>
+                
+                </div>
+                <div class="grid-item">
+                <div className = "flex"> <span>2 Jun 2021</span></div>
+                   
+                </div>
+                <div class="grid-item">
+                <div className = "flex"><span>$1200</span></div></div>
+                <div class="grid-item">
+                <div className = "flex"><span>Approved</span></div></div>
+
+                <div class="grid-item">
+                <div className = "flex">
+                <img src = "https://cdn.pixabay.com/photo/2021/07/14/19/18/woman-6466907__340.jpg" alt ="tell all the boys"></img>
+                <span>Susan Carol</span>
+                </div>
+                
+                </div>
+                <div class="grid-item">
+                <div className = "flex"> <span>2 Jun 2021</span></div>
+                   
+                </div>
+                <div class="grid-item">
+                <div className = "flex"><span>$1200</span></div></div>
+                <div class="grid-item">
+                <div className = "flex"><span>Approved</span></div></div>
+
+                <div class="grid-item">
+                <div className = "flex">
+                <img src = "https://cdn.pixabay.com/photo/2021/07/14/19/18/woman-6466907__340.jpg" alt ="tell all the boys"></img>
+                <span className = "gridusername">Susan Carol</span>
+                </div>
+                
+                </div>
+                <div class="grid-item">
+                <div className = "flex"> <span>2 Jun 2021</span></div>
+                   
+                </div>
+                <div class="grid-item">
+                <div className = "flex"><span>$1200</span></div></div>
+                <div class="grid-item">
+                <div className = "flex"><span>Approved</span></div></div>
             </div>
         </div>
     )
